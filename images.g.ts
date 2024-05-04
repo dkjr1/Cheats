354fea853843bd19dd946bf62481e4e82227a0c4
dkjr1 <73570166+dkjr1@users.noreply.github.com>
2024-05-04 13:32:50 -0700

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,44 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "myImage":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . 4 4 4 4 4 . . . . . . . . 
+. . 4 5 5 5 5 5 4 . . . . . . . 
+. . 4 5 4 4 4 5 4 . . . . . . . 
+. . 4 5 4 5 4 5 4 . . . . . . . 
+. . 4 5 5 5 4 5 4 . . . . . . . 
+. . 4 4 4 4 4 5 4 . . 7 . . . 7 
+. . . 5 5 5 5 5 . . . . 7 . 7 . 
+. . . 7 7 7 7 7 7 7 7 7 7 7 . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
